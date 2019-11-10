199706830ab08509ff5c1de680aeb5e617242ec9
Pavel Sychev <pasha.sychev@gmail.com>
2019-11-10 16:35:46 +0100

    Add term command.

---
+++ cmd/term/term.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+
+	term "github.com/nsf/termbox-go"
+)
+
+func main() {
+	if err := term.Init(); err != nil {
+		log.Fatalf("term.Init(): %v", err)
+	}
+	term.SetInputMode(term.InputEsc)
+
+	defer term.Close()
+
+	// Fill the
+	termH, termW := term.Size()
+	for i := 0; i < termH; i++ {
+		for j := 0; j < termW; j++ {
+			term.SetCell(i, j, '█', term.ColorBlue, term.AttrBold)
+		}
+	}
+
+	// After we change colors we need to Flush
+	// to actually draw something.
+	term.Flush()
+
+	// Cursor position for drawing.
+	var h, w int
+	for {
+		ev := term.PollEvent()
+		switch ev.Type {
+		case term.EventError:
+			log.Fatal(ev.Err)
+		case term.EventKey:
+		default:
+			continue
+		}
+
+		switch ev.Key {
+		case term.KeyEsc:
+			// Exit on espace.
+			return
+		case term.KeyArrowDown:
+			h += 2
+		case term.KeyArrowLeft:
+			w -= 2
+		case term.KeyArrowRight:
+			w += 2
+		case term.KeyArrowUp:
+			h -= 2
+		default:
+			continue
+		}
+		term.SetCell(w, h, 'x', term.ColorBlack, term.AttrReverse)
+		term.Flush()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/luckygeck/goplay
+
+go 1.13
+
+require (
+	github.com/mattn/go-runewidth v0.0.6 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190817171036-93860e161317
+)
